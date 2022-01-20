6531fd285d2d0144276a47c2c33e96e900579a71
jianlong <72846298+ljl0012@users.noreply.github.com>
2022-01-21 00:58:43 +0800

    Update Viewshed.java

--- src/main/java/analysis/Viewshed.java
+++ src/main/java/analysis/Viewshed.java
@@ -25,7 +25,7 @@ public class Viewshed {
             }
         }
 
-        Point startPoint=new Point(pointCoordinateX,pointCoordinateY);
+        Point startPoint=new Point(pointCoordinateX+0.5,pointCoordinateY+0.5);
 
         if(Dem[(int) pointCoordinateX][(int) pointCoordinateY]==Nodata)
             System.out.println("The point is not exist");
@@ -42,9 +42,9 @@ public class Viewshed {
                         ArrayList<Point> temList = new ArrayList<Point>();
                         ArrayList<Point> gridList = new ArrayList<Point>();
 //                        HashSet<Point> hs=new HashSet<Point>();
-                        Point endPoint = new Point(i, j);
+                        Point endPoint = new Point(i+0.5, j+0.5);
 
-                        double dx = endPoint.X - startPoint.X;
+                       double dx = endPoint.X - startPoint.X;
                         double dy = endPoint.Y- startPoint.Y;
 
                         // 设置增量
@@ -54,19 +54,32 @@ public class Viewshed {
                         double k = dy / dx;
                         double b = -k * startPoint.X + startPoint.Y;
                         // 开始计算交点
-                        for (int m = 0; m <= Math.abs(dx) - 1; m++) {
+                        for (int m = 0; m <= Math.abs(dx)-1; m++) {
                             // 沿横轴方向遍历，既查询与网格轴轴的交点
                             Point temPoint1 = new Point();
-                            temPoint1.X = Math.ceil(startPoint.X) + increX * m;
+                            if(dx>0)
+                                temPoint1.X = Math.ceil(startPoint.X) + increX * m;
+                            else if (dx<0)
+                                temPoint1.X = Math.floor(startPoint.X) + increX * m;
+                            else
+                                temPoint1.X = startPoint.X + increX * m;
                             temPoint1.Y = k * temPoint1.X + b;
                             temList.add(temPoint1);
                         }
                         // 沿纵轴方向遍历，既查询与网格横轴的交点
-                        for (int n = 0; n <= Math.abs(dy); n++) {
+                        for (var n = 0; n <= Math.abs(dy)-1; n++) {
                             Point temPoint2 = new Point();
-                            temPoint2.Y = Math.ceil(startPoint.Y) + increY * n;
+                            if (dy>0)
+                                temPoint2.Y = Math.ceil(startPoint.Y) + increY * n;
+                            else if(dy<0)
+                                temPoint2.Y = Math.floor(startPoint.Y) + increY * n;
+                            else
+                                temPoint2.Y = startPoint.Y + increY * n;
                             temPoint2.X = (temPoint2.Y - startPoint.Y) / k + startPoint.X;
                             temList.add(temPoint2);
+
+//                        int max=Dem[i][j];
+//                        for int(m=i,m)
                         }
 
                         for (int kk=0; kk<(temList.size());kk++) {
@@ -74,35 +87,45 @@ public class Viewshed {
                             Point temGridDown = new Point();
                             Point temGridLeft = new Point();
                             Point temGridRight = new Point();
+                            Point temGridCenter = new Point();
                             // 横轴交点
                             if((temList.get(kk).X !=Math.ceil(temList.get(kk).X)) &&
                                     (temList.get(kk).Y ==Math.ceil(temList.get(kk).Y))){
                                 temGridUp.X = Math.ceil(temList.get(kk).X);
-                                temGridUp.Y = Math.ceil(temList.get(kk).X)+1;
+                                temGridUp.Y = Math.ceil(temList.get(kk).Y)+1;
                                 temGridDown.X = Math.ceil(temList.get(kk).X);
-                                temGridDown.Y = Math.ceil(temList.get(kk).X);
+                                temGridDown.Y = Math.ceil(temList.get(kk).Y);
                                 gridList.add(temGridDown);
                                 gridList.add(temGridUp);
                             }
                             // 纵轴交点
                             else if((temList.get(kk).Y !=Math.ceil(temList.get(kk).Y)) &&
                                     (temList.get(kk).X ==Math.ceil(temList.get(kk).X))) {
-                                temGridLeft.X = Math.ceil(temList.get(kk).Y);
+                                temGridLeft.X = Math.ceil(temList.get(kk).X);
                                 temGridLeft.Y = Math.ceil(temList.get(kk).Y);
-                                temGridRight.X = Math.ceil(temList.get(kk).Y)+1;
+                                temGridRight.X = Math.ceil(temList.get(kk).X)+1;
                                 temGridRight.Y = Math.ceil(temList.get(kk).Y);
                                 gridList.add(temGridLeft);
                                 gridList.add(temGridRight);
+                            }
+                            else if((temList.get(kk).Y ==Math.ceil(temList.get(kk).Y)) &&
+                                    (temList.get(kk).X ==Math.ceil(temList.get(kk).X))) {
+                                temGridCenter.X = Math.ceil(temList.get(kk).X+0.5*increX);
+                                temGridCenter.Y = Math.ceil(temList.get(kk).Y+0.5*increY);
+                                gridList.add(temGridCenter);
                             }
                         }
 
 
                         double max=-9999;
+                        double max=Dem[i][j];
                         for(int kk=0;kk<gridList.size();kk++){
-                            if(Dem[(int) gridList.get(kk).X][(int) gridList.get(kk).Y]>max)
-                                max=Dem[(int) gridList.get(kk).X][(int) gridList.get(kk).Y];
+                            if((gridList.get(kk).X-1==x1&& gridList.get(kk).Y-1==y1)||(gridList.get(kk).X-1==i&& gridList.get(kk).Y-1==j))
+                                continue;
+                            else if(Dem[(int) gridList.get(kk).X-1][(int) gridList.get(kk).Y-1]>max)
+                                max=Dem[(int) gridList.get(kk).X-1][(int) gridList.get(kk).Y-1];
                         }
-                        if(max>=Dem[i][j])
+                        if(max>Dem[i][j])
                             view[i][j]=0;
                         else {
                             view[i][j] = 1;
